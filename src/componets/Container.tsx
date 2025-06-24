647b4635ca3bd988d2a1e674b734112ea4a4b0d3
rajgupta-ml <pratikrajgupta23@gmail.com>
2025-06-24 22:23:46 +0530

    Refactor Landing component to include Hero section and update Container styles for full height

--- src/componets/Container.tsx
+++ src/componets/Container.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Container = ({ children }: { children: React.ReactNode }) => {
-	return <div className="max-w-7xl mx-auto p-4">{children}</div>;
+	return <div className="max-w-7xl mx-auto  h-full">{children}</div>;
 };
 
 export default Container;
--- src/componets/Landing.tsx
+++ src/componets/Landing.tsx
@@ -1,13 +1,43 @@
 import Navbar from '@/componets/Navbar';
 import React from 'react';
 import Container from './Container';
-
+import Link from 'next/link';
+import { ArrowRight } from 'lucide-react';
 const Landing = () => {
 	return (
-		<div>
+		<div className="max-h-screen h-screen">
 			<Container>
-				<Navbar />
+				<div className="border-r border-l h-full p-4 ">
+					<Navbar />
+					<Hero />
+				</div>
 			</Container>
+		</div>
+	);
+};
+
+const Hero = () => {
+	return (
+		<div className=" h-full flex flex-col gap-2 items-center justify-center">
+			<span className="text-primary">No code Needed</span>
+			<div className="text-center flex flex-col gap-10 mb-8">
+				<h1 className="text-8xl font-bold text-wrap">
+					Customizable checkout pages that convert
+				</h1>
+				<p className="text-xl  ">
+					Accept payments with 0% transaction fees on any website,powered by
+					Stripe - without needing to code
+				</p>
+			</div>
+			<div className="w-full  items-center flex justify-center ">
+				<Link
+					href="/signup"
+					className=" bg-primary text-muted w-md text-center flex items-center justify-center p-4"
+				>
+					Sign Up
+					<ArrowRight className="" />
+				</Link>
+			</div>
 		</div>
 	);
 };
